89cd771e73b5a7681f0d16432f5582b812938eb4
Greyson Jones <rgreysonjones@gmail.com>
2024-04-16 17:47:48 -0400

    backend minimally talks to frontend

--- src/pages/MultipleView.jsx
+++ src/pages/MultipleView.jsx
@@ -1,18 +1,35 @@
 import { Grid, Stack } from "@mui/material";
 import Profile from "../components/Profile"
 import TopBar from "../components/TopBar"
+import axios from 'axios'
 import './MultipleView.css';
+import { useEffect, useState } from "react";
+
 const MultipleView = () => {
+  const [animals, setAnimals] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/cattle/')
+    .then(
+      response => {
+        setAnimals(response.data.data[0]);
+      })
+    .catch(error => setError(error))
+
+  }, []);
+
+  useEffect(() => {
+    console.log(animals);
+  }, [animals]);
+
   return (
     <div>
       <TopBar></TopBar>
       <Stack>
-          <Profile name="Bessie" age="4" weight="1000" tag="1234"/>
-          <Profile name="Sarah" age="3" weight="1234" tag="1235"/>
-          <Profile name="Cow" age="5" weight="1100" tag="1236"/>
-          <Profile name="More Cow" age="6" weight="1150" tag="1237"/>
-          <Profile name="Yet Another Cow" age="3" weight="975" tag="1238"/>
-          <Profile name="One Final Cow" age="4" weight="5000" tag="1239"/>
+        {animals && animals.map((animal) => (
+          <Profile tag={animal.tag_number} />
+        ))}
       </Stack>
     </div>
   )
